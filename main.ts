a5fea409a5e9df56062abf253394b0ba41cbd35f
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-06-14 16:59:41 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 //% color=#188634
 namespace imageio {
+    //% block="5×5"
+    //% weight=100
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=5 imageLiteralColumns=5
+    export function createImage5x5(leds: string): Image {
+        return <Image><any>leds
+    }
 }
